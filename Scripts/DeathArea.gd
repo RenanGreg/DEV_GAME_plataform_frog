d4afa40c925ff6aa23565f43e037c1baf1cce5a0
Renan Gregório <renangreg@outlook.com>
2024-09-23 09:49:31 -0300

    ui ui

--- Scripts/DeathArea.gd
+++ Scripts/DeathArea.gd
@@ -5,6 +5,8 @@ func _on_body_entered(body):
 	
 	if body.name == "Player": 
 		Global.life -= 1 
+		Global.score = 0 
+		
 		await get_tree().create_timer(0.5).timeout 
 		
 		get_tree().reload_current_scene() 
